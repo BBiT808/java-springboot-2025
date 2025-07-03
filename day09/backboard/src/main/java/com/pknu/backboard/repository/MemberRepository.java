7bf3e501066b01c03022b8ea78a822156b2811de
BBiT808 <witchsparadise11003311@gmail.com>
2025-07-03 16:50:10 +0900

    20250703 day09 수정

--- day09/backboard/src/main/java/com/pknu/backboard/repository/MemberRepository.java
+++ day09/backboard/src/main/java/com/pknu/backboard/repository/MemberRepository.java
@@ -1,18 +1,17 @@
 package com.pknu.backboard.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.pknu.backboard.entity.Member;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     // SELECT * FROM member WHERE username = ?
-    List<Member> findByUsername(String username); // 사용자 이름으로 조회
+    Optional<Member> findByUsername(String username); // 사용자이름으로 사용자 조회
 
-    Optional<Member> findByEmail(String emial); // 이메일로 사용자 조회
+    Optional<Member> findByEmail(String email); // 이메일로 사용자 조회
 }
--- day09/backboard/src/main/java/com/pknu/backboard/service/MemberService.java
+++ day09/backboard/src/main/java/com/pknu/backboard/service/MemberService.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,29 +17,29 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MemberService {
 
+    @Autowired
     private final MemberRepository memberRepository;
 
     public Member setMember(String username, String email, String password) {
         Member member = new Member();
         member.setUsername(username);
         member.setEmail(email);
-        // member.setPassword(); //12345 가 그대로 들어감 !! ; 사용 안 해!!
+        // member.setPassword(password); // 12345 가 그대로 들어감
         BCryptPasswordEncoder pwdEnc = new BCryptPasswordEncoder();
-        member.setRegDate(LocalDateTime.now()); // 날짜 저장 추가!!
-        member.setPassword(pwdEnc.encode(password)); // 12345 -> EFWEFER@##$$REASDF!@#$1234... 등과 같이 암호화!
+        member.setRegDate(LocalDateTime.now()); // 날짜저장 추가
+        member.setPassword(pwdEnc.encode(password)); // 12345 -> EFWEFER@#$$@#$FEFG#@324423WR 와 같이 암호화
 
         this.memberRepository.save(member);
         return member;
     }
 
-    // 같은 이름의 사용자 존재여부 파악 메서드 !
+    // 같은 이름의 사용자 존재여부 파악 메서드
     public boolean getExistMember(String username) {
-        List<Member> opMember = this.memberRepository.findByUsername(username);
-        if (!opMember.isEmpty()) {
+        Optional<Member> opMember = this.memberRepository.findByUsername(username);
+        if (opMember.isPresent()) {
             return true; // 같은 아이디 사용자 존재
         } else {
-            return false; // 없음!! 회원 가입 가능 !!
+            return false; // 없음. 회원가입 가능
         }
     }
-
 }
